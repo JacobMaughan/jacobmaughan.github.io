e74842db0476252b1beab4ba0d4bcec6af1425b7
Jacob <jmaughan1024@gmail.com>
2021-02-19 16:37:53 -0600

    Fixed contact form

---
+++ assets/js/form.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("contactForm");
+
+const nameBox = document.getElementById("name");
+const emailBox = document.getElementById("email");
+const messageBox = document.getElementById("message");
+
+form.addEventListener("submit", function(event) {
+    i
+});
