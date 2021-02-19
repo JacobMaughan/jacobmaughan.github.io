e01cf2497e627f4aceec6a5d2c96ed98a92a4c67
Jacob <jmaughan1024@gmail.com>
2021-02-19 16:46:53 -0600

    Removed JS

--- assets/js/form.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("contactForm");
-
-const nameBox = document.getElementById("name");
-const emailBox = document.getElementById("email");
-const messageBox = document.getElementById("message");
-
-form.addEventListener("submit", function(event) {
-    i
-});
