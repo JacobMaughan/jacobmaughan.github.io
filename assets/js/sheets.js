6376bf139012a132720729f4628743c404d21195
Jacob Maughan <jmaughan1024@gmail.com>
2023-07-22 18:42:34 -0500

    testing

--- assets/js/sheets.js
+++ assets/js/sheets.js
@@ -15,7 +15,7 @@ function get_posts() {
 
 function get_article(page_id) {
 	let sheet_title = page_id;
-	let sheet_range = 'A2:D10';
+	let sheet_range = 'A2:D2';
 	
 	let url = 'https://docs.google.com/spreadsheets/d/' + sheet_id + '/gviz/tq?sheet=' + sheet_title + '&range=' + sheet_range
 	
@@ -24,6 +24,12 @@ function get_article(page_id) {
 	.then(rep => {
 		let data = JSON.parse(rep.substr(47).slice(0, -2));
 		
-		document.title = 'Jacob Maughan - ' + data.table.rows[0].c[1].v;
+		document.title = 'Jacob Maughan - ' + data.table.rows[0].c[0].v;
+		document.getElementById('article_title').innerHTML = data.table.rows[0].c[0].v;
+		document.getElementByID('article_headline').innerHTML = data.table.rows[0].c[1].v;
+		document.getElementByID('article_published_date').innerHTML = data.table.rows[0].c[2].v;
+		document.getElementByID('article_body').innerHTML = data.table.rows[0].c[3].v;
+		
+		
 	})
 }
