b0f9b97d03f4017f7b06dd8ccbcbfec152b0591d
Jacob Maughan <jmaughan1024@gmail.com>
2023-07-23 06:46:56 -0500

    Updated Home Page:

--- assets/js/sheets.js
+++ assets/js/sheets.js
@@ -1,5 +1,42 @@
 let sheet_id = '1L0mPMkaarKGy_WHrdD49TvBV82xb1SCOwsWnRKotN3Y'
 
+function get_recent_posts() {
+	let sheet_title = 'blog_main';
+	let sheet_range = 'A2:D5';
+	
+	let url = 'http://docs.google.com/spreadsheets/d/' + sheet_id + '/gviz/tq?sheet=' + sheet_title + '&range=' + sheet_range
+	
+	fetch(FULL_URL)
+	.then(res => res.text())
+	.then(rep => {
+		let data = JSON.parse(rep.substr(47).slice(0, -2));
+		
+		document.getElementById('article1_mainlink').href = 'article?id=' + data.table.rows[0].c[0].v;
+		document.getElementById('article1_title').href = 'article?id=' + data.table.rows[0].c[0].v;
+		document.getElementById('article1_title').innerHTML = data.table.rows[0].c[1].v;
+		document.getElementById('article1_heading').innerHTML = data.table.rows[0].c[3].v;
+		document.getElementById('article1_image').src = data.table.rows[0].c[2].v;
+		
+		document.getElementById('article2_mainlink').href = 'article?id=' + data.table.rows[1].c[0].v;
+		document.getElementById('article2_title').href = 'article?id=' + data.table.rows[1].c[0].v;
+		document.getElementById('article2_title').innerHTML = data.table.rows[1].c[1].v;
+		document.getElementById('article2_heading').innerHTML = data.table.rows[1].c[3].v;
+		document.getElementById('article2_image').src = data.table.rows[1].c[2].v;
+		
+		document.getElementById('article3_mainlink').href = 'article?id=' + data.table.rows[2].c[0].v;
+		document.getElementById('article3_title').href = 'article?id=' + data.table.rows[2].c[0].v;
+		document.getElementById('article3_title').innerHTML = data.table.rows[2].c[1].v;
+		document.getElementById('article3_heading').innerHTML = data.table.rows[2].c[3].v;
+		document.getElementById('article3_image').src = data.table.rows[2].c[2].v;
+		
+		document.getElementById('article4_mainlink').href = 'article?id=' + data.table.rows[3].c[0].v;
+		document.getElementById('article4_title').href = 'article?id=' + data.table.rows[3].c[0].v;
+		document.getElementById('article4_title').innerHTML = data.table.rows[3].c[1].v;
+		document.getElementById('article4_heading').innerHTML = data.table.rows[3].c[3].v;
+		document.getElementById('article4_image').src = data.table.rows[3].c[2].v;
+	})
+}
+
 function get_posts() {
 	let sheet_title = 'blog_main';
 	let sheet_range = 'A2:D50';
