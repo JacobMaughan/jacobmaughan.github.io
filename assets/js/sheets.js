d5c5d7e73b5ce60f0a1d6dfc1e189b73278c66c8
Jacob Maughan <jmaughan1024@gmail.com>
2023-07-23 05:55:39 -0500

    fixed date

--- assets/js/sheets.js
+++ assets/js/sheets.js
@@ -24,12 +24,13 @@ function get_article(page_id) {
 	.then(rep => {
 		let data = JSON.parse(rep.substr(47).slice(0, -2));
 		
+		date = data.table.rows[0].c[2].v;
+		
 		document.title = 'Jacob Maughan - ' + data.table.rows[0].c[0].v;
 		document.getElementById('article_title').innerHTML = data.table.rows[0].c[0].v;
 		document.getElementById('article_headline').innerHTML = data.table.rows[0].c[1].v;
-		document.getElementById('article_published_date').innerHTML = data.table.rows[0].c[2].v;
+		document.getElementById('article_published_date').innerHTML = date.substr(9,3).replace(/,/g, '') + '/' + date.substr(date.length - 3, 2).replace(/,/g, '') + '/' + date.substr(5, 4);
 		document.getElementById('article_body').innerHTML = data.table.rows[0].c[3].v;
-		
 		
 	})
 }
